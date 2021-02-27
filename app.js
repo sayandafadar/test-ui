fc5a7eb5c07d545f940d85693c5e7b254926363e
Sayan Dafadar <77375022+sayandafadar@users.noreply.github.com>
2021-02-27 12:04:24 +0530

    Added
    Test

---
+++ app.js
@@ -0,0 +1,37 @@
+// // let newItem = "new";
+
+// let todoList = ['Collect Egg'];
+
+// let commandPrompt = true;
+
+// let i = 0;
+
+// // prompt("What would you like to do?")
+
+// while (commandPrompt) {
+//     let newItem = prompt("What would you like to do? Type 'new' to add new");
+//     if (newItem === "new") {
+//         let newTodo = prompt("Add a new todo");
+//         todoList.push(newTodo);
+//         console.log(`${todoList[i]} is added to the list`);
+//         i += 1;
+//     } else if (newItem === "list") {
+//         console.log('******************')
+//         for (let j = 0; j < todoList.length; j++) {
+//             console.log(`${j}: ${todoList[j]}`);
+//         }
+//         console.log('******************')
+//     } else if (newItem === "delete") {
+//         const index = prompt("OK! enter an index to delete")
+//         const deleted = todoList.splice(index, 1);
+//         console.log(`OK! deleted ${deleted[0]}`)
+
+//     } 
+    
+//     else if (newItem === "quit" || newItem === "q") {
+//         console.log('OK! YOU QUIT THE APP')
+//         break;
+//     } 
+// }
+
+
